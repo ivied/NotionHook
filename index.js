c3d8d1b9a579e129a287e6190ed189bdcc4a2d52
Eder Martínez <63567815+JackBlaze132@users.noreply.github.com>
2024-04-11 20:16:14 -0500

    test_04 notion hook get user

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function createCommit(notion, commits) {
             {
               type: 'text',
               text:{
-                content:commit.committer
+                content:commit.author.name
               }
             }
           ]
